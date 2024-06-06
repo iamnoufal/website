3f3e3027cb6e3374aadece2c5603924d5f314ca3
Noufal <jnrahman@gmail.com>
2024-06-06 11:28:33 +0530

    added bitmoji image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,7 @@ import { montserrat, lora } from "@/theme/fonts";
 import PortfolioNav from "@/components/PortfolioNav";
 import Theme from "@/theme/Provider";
 import Footer from "@/components/Footer";
-import favicon from "@/assets/images/favicon.ico"
 import ScrollBehaviour from "@/helpers/ScrollBehaviour";
-
-export const metadata: Metadata = {
-  icons: {
-    icon: favicon.src
-  }
-}
 
 export default function RootLayout({
   children,
