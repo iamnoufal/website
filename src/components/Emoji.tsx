79e312bb05d16a502a898075bd9b88587d87dd51
iamnoufal <iam@noufal.dev>
2026-03-03 10:13:36 +0530

    v4 existing design and old next config cleanup

--- src/components/Emoji.tsx
+++
@@ -1,8 +0,0 @@
-import { montserrat } from "@/theme/fonts"
-import { CSSProperties } from "react"
-
-const Emoji = ({ children, style } : { children: React.ReactNode, style?: CSSProperties}) => {
-  return <span role="img" aria-label="emoji" style={style} className={montserrat.className}>{children}</span>
-}
-
-export default Emoji
--- src/components/ScrollBehaviour.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import ReactLenis, { useLenis } from "lenis/react";
-
-export default function ScrollBehaviour ({ children }: { children: React.ReactNode }) {
-  const lenis = useLenis();
-  return <ReactLenis root>{children}</ReactLenis>;
-};
